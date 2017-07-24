59c8343f07d8d51105e89493282410da63909f10
minseok <minseok@proviso.com.au>
2017-07-24 17:02:38 +0930

    first commit

--- tutorial1/ui.R
+++ tutorial1/ui.R
@@ -1,22 +1,26 @@
 library(shiny)
 
 shinyUI(fluidPage(
-  titlePanel("title panel"),
+  titlePanel("My First Shiny App"),
 
   sidebarLayout(
-    sidebarPanel(),
+    sidebarPanel(position = "top",
+      strong(h3("Installation")),
+      p("shiny is available on CRAN, so you can install it in the usual way from your R console.")
+    ),
     mainPanel(
-      p("p creates a paragraph of text."),
-      p("A new p() command starts a new paragraph. Supply a style attribute to change the format of the entire paragraph.", style = "font-family: 'times'; font-si16pt"),
-      strong("strong() makes bold text."),
-      em("em() creates italicized (i.e, emphasized) text."),
-      br(),
-      code("code displays your text similar to computer code"),
-      div("div creates segments of text with a similar style. This division of text is all blue because I passed the argument 'style = color:blue' to div", style = "color:blue"),
-      br(),
-      p("span does the same thing as div, but it works with",
-        span("groups of words", style = "color:blue"),
-        "that appear inside a paragraph.")
+      # p("p creates a paragraph of text."),
+      # p("A new p() command starts a new paragraph. Supply a style attribute to change the format of the entire paragraph.", style = "font-family: 'times'; font-si16pt"),
+      # strong("strong() makes bold text."),
+      # em("em() creates italicized (i.e, emphasized) text."),
+      # br(),
+      # code("code displays your text similar to computer code"),
+      # div("div creates segments of text with a similar style. This division of text is all blue because I passed the argument 'style = color:blue' to div", style = "color:blue"),
+      # br(),
+      # p("span does the same thing as div, but it works with",
+      #   span("groups of words", style = "color:blue"),
+      #   "that appear inside a paragraph.")
+      img(src = "bigorb.png", height = 300, width = 300)
     )
   )
 ))
